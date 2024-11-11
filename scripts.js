5aaeacf245b0ed0133640e4b7897598380641056
Juan Pina <juanpinaribeiro@outlook.com>
2024-11-11 17:04:56 -0300

    Exibindo o footer (que mostra o resultado da conversão)

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const GBP = 6.08
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 // Manipulando o input amount para receber apenas números
 amount.addEventListener("input", () => {
@@ -34,5 +35,14 @@ form.onsubmit = (event) => {
 
 // Função para converter a moeda
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol)
+  try {
+    // Aplica a classe que exibe o footer para mostrar o resultado
+    footer.classList.add("show-result")
+  } catch (error) {
+    // Remove a classe que exite o footer, ocultando-o da tela
+    footer.classList.remove("show-result")
+    
+    console.log(error)
+    alert("Não foi possível efetuar a conversão. Por gentileza, tente novamente mais tarde!")
+  }
 }
